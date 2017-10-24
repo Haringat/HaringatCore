d28fb0a77909ed5bb0c39d62848151c5ce5f9d8a
Marcel Mundl <Marcel@Mundlhome.de>
2017-10-25 00:02:41 +0200

    initial commit

---
+++ src/main/kotlin/com/github/haringat/haringatCore/HaringatCore.kt
@@ -0,0 +1,12 @@
+package com.github.haringat.haringatCore
+
+import cpw.mods.fml.common.Mod
+
+@Mod(
+        modid = "haringat-core",
+        name = "Haringat Core",
+        version = "1.0.0",
+        dependencies = "after:forgelin"
+)
+object HaringatCore {
+}
